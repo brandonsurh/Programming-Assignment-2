63c85346631a285ae6b5b2761f8096ef989e8f67
brandonsurh <89608157+brandonsurh@users.noreply.github.com>
2022-03-04 23:10:13 -0500

    Add files via upload

---
+++ PA2.java
@@ -0,0 +1,235 @@
+
+
+// create class for threads
+// PRIMARY CLASS
+//  - chance to reach end
+//  - eats cake only one time when reaching end
+//  - after cake was eaten once, dont eat anymore
+// make secondary class for volunteer thread who will change cakes
+// SECONDARY CLASS
+//  - only person to request cakes
+//  - counts amount of times cake is requested
+//  - tells minotaur everyone has visited when requests reachs n-1 guests (request guest amount of times -1)
+//  - prints that everyone has visited and possibly ends program
+
+// threads will have probability to find way out of maze and reach cake
+
+// MAIN
+// start n-1 primary threads
+// start 1 secondary thread
+// all threads are sleeping
+// minotaur wakes one random thread at a time
+// simulation only stops when secondary class announces everyone has visited
+
+import java.util.*;
+
+public class PA2 {
+
+    
+
+    public static void main (String [] args) {
+
+        Random rand = new Random();
+
+        Scanner sc = new Scanner(System.in);  
+        System.out.println("Enter number of guests: ");
+        int totalGuests = sc.nextInt();
+
+        // array to check if thread was already started or not
+        Boolean [] started = new Boolean[totalGuests];
+        Arrays.fill(started, Boolean.FALSE);
+
+        PrimaryGuest [] primGuests = new PrimaryGuest[totalGuests - 1];
+        SecondaryGuest secGuest = new SecondaryGuest(totalGuests);
+
+        // create threads
+        for (int i = 0; i < totalGuests - 1; i++)
+        {
+            primGuests[i] = new PrimaryGuest(i);
+        }
+
+
+        int randomNumber;
+        // while some threads havent been started, keep trying to start threads
+        while (Arrays.asList(started).contains(Boolean.FALSE) == true)
+        {
+            //System.out.println(Arrays.toString(started));
+            
+
+            randomNumber = rand.nextInt(totalGuests);
+            //System.out.println("randomNumber in first while loop is " + randomNumber);
+            
+            // chose designated thread and not started yet
+            if (randomNumber == totalGuests - 1 && Arrays.asList(started).get(randomNumber) == false)
+            {
+                secGuest.start();
+                try{
+                    secGuest.join();
+                } catch(InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                }
+                started[randomNumber] = Boolean.TRUE;
+            }
+            // if thread chosen hasnt started yet
+            else if (randomNumber != totalGuests - 1 && Arrays.asList(started).get(randomNumber) == false)
+            {
+                primGuests[randomNumber].start();
+                try{
+                    primGuests[randomNumber].join();
+                } catch(InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                }
+                started[randomNumber] = Boolean.TRUE;
+            }
+        }
+        //System.out.println("Finished start while loop");
+        // while loop to keep running threads
+        while (true)
+        {
+            randomNumber = rand.nextInt(totalGuests);
+            //System.out.println("randomNumber in second while loop is " + randomNumber);
+
+            if (randomNumber == totalGuests - 1)
+            {
+                // run thread
+                secGuest.run();
+                try{   
+                    secGuest.join();
+                } catch(InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+            else
+            {
+                // run thread
+                primGuests[randomNumber].run();
+                try{   
+                    primGuests[randomNumber].join();
+                } catch(InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
+
+class Labyrinth {
+    public static boolean isCakePresent = true;
+}
+
+// these are the guests who eat cake
+class PrimaryGuest extends Thread {
+
+    Random rand = new Random();
+
+    private int threadNumber;
+    private boolean hadCake;
+
+    // constructor
+    public PrimaryGuest (int threadNumber)
+    {
+        this.threadNumber = threadNumber;
+        this.hadCake = false;
+    }
+
+    @Override
+    public void run()
+    {
+        //System.out.println("Thread " + threadNumber + " is awake.");
+
+        // decides if guest makes it to end of maze
+        int randomNumber = rand.nextInt(10);
+
+        // guest reaches end
+        if (randomNumber > 2)
+        {
+            //System.out.println("Thread " + threadNumber + " reached the cake.");
+            //System.out.println("isCakePresent is currently" + Labyrinth.isCakePresent);
+            // if guest did not have cake and cake is there, eat cake
+            if (hadCake == false && Labyrinth.isCakePresent == true)
+            {
+                // eat cake, set true 
+                this.hadCake = true;
+
+                // set atomic variable? for cake to be missing state
+                Labyrinth.isCakePresent = false;
+                //System.out.println("Thread " + threadNumber + " ate the cake.");
+                //System.out.println("isCakePresent is now " + Labyrinth.isCakePresent);
+            }
+            else
+            {
+                //System.out.println("Thread " + threadNumber + " did not eat the cake.");
+            }
+        }
+        // guest does not reach end
+        else
+        {
+            //System.out.println("Thread " + threadNumber + " did not reach the cake.");
+            // guest did not reach end
+            // may not need else statement
+        }
+
+        // thread needs to sleep and then new thread needs to run
+    }
+}
+
+// this is the designated guest who counts cakes eaten
+class SecondaryGuest extends Thread {
+
+    Random rand = new Random();
+
+    private int requestCount;
+    private int totalGuests;
+
+    // constructor given amount of total guests
+    public SecondaryGuest(int totalGuests)
+    {
+        // set amount of requests to zero
+        this.requestCount = 0;
+        this.totalGuests = totalGuests;
+    }
+
+    @Override
+    public void run()
+    {
+
+        //System.out.println("Designated counter thread is awake.");
+
+        // decides if guest makes it to end of maze
+        int randomNumber = rand.nextInt(10);
+
+        // guest reaches end
+        if (randomNumber > 2)
+        {
+            //System.out.println("Designated counter reached the end.");
+            // if there is no cake, someone ate it
+            // request more cake, increment
+            if (Labyrinth.isCakePresent == false)
+            {
+                // request more cake, increment
+                requestCount++;
+                // atomic variable? (isCakePresent), set to true
+                Labyrinth.isCakePresent = true;
+                //System.out.println("Designated counter requested cake." + " requestCount is now " + requestCount);
+            }
+        }
+        // guest does not reach end
+        else
+        {
+            //System.out.println("Designated counter did not reach the end.");
+            // guest did not reach end
+            // may not need else statement
+        }
+
+        // if requestCount is equal to n-1, then all guests have had cake and all have visited
+        if (requestCount == totalGuests - 1)
+        {
+            System.out.println("All guests have visited the labyrinth!");
+            // needs to shutdown program
+            System.exit(0);
+        }
+
+        // thread needs to sleep and then new thread needs to run
+    }
+}
+
---
+++ PA2_part2.java
@@ -0,0 +1,109 @@
+
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.*;
+import java.util.concurrent.atomic.*;
+
+public class PA2_part2 {
+
+	
+
+	public static void main (String [] args) {
+
+		Random rand = new Random();
+		
+
+		Scanner sc = new Scanner(System.in);  
+		System.out.println("Enter number of guests: ");
+		int totalGuests = sc.nextInt();
+
+		Guest [] guests = new Guest[totalGuests];
+
+		// array to check if thread was already started or not
+        Boolean [] started = new Boolean[totalGuests];
+        Arrays.fill(started, Boolean.FALSE);
+		SpinLock lock = new SpinLock();
+
+		for (int i = 0; i < totalGuests; i++)
+		{
+			
+			guests[i] = new Guest(i, lock);
+		}
+
+		for (int i = 0; i < totalGuests; i++)
+		{
+			
+			guests[i].start();
+		}
+
+		while (true)
+		{
+			
+			guests[rand.nextInt(totalGuests - 1)].run();
+		}
+		
+	}
+}
+
+
+
+class Guest extends Thread {
+
+	//private boolean sawVase;
+	private int threadNumber;
+	private int queueChance;
+	SpinLock lock;
+
+	Random rand = new Random();
+
+	// constructor
+	public Guest (int threadNumber, SpinLock lock)
+	{
+		//this.sawVase = false;
+		this.queueChance = 2;
+		this.lock = lock;
+		this.threadNumber = threadNumber;
+
+	}
+
+	public void run() 
+	{
+		
+		// if random number 1-100 is more than queue chance times 100 (100, 50, 25, )
+		//System.out.println("running thread " + threadNumber);
+		if (rand.nextInt(100* queueChance) < 50 && queueChance < 1024)
+		{
+			//System.out.println("Guest " + threadNumber + " check room. Queue chance is " + queueChance);
+			queueChance = queueChance * 2;
+			// tries lock
+			lock.lock();
+			//System.out.println("Guest " + threadNumber + " locked room");
+			//System.out.println("Hooray! Guest " + threadNumber + " is in the room. Next chance is " + queueChance);
+			lock.unlock();
+			//System.out.println("Guest " + threadNumber + " unlocked");
+		}
+		//else
+			//System.out.println("did not pass check");
+
+	}
+}
+
+class SpinLock extends ReentrantLock{
+
+	public SpinLock() {
+		super();
+	}
+
+	public void lock() {
+		while (!super.tryLock())
+		{
+			//System.out.println("I am waiting");
+			// Do Nothing
+		}
+	}
+
+	public void unlock()
+	{
+		super.unlock();
+	}
+}
